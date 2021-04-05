698417701223bdedbfd9572b0d3567211f3ee78d
Prakhar Gurawa <prakhargurawa@gmail.com>
2021-04-05 20:07:48 +0530

    Update keras-seq2seq.py

--- model3/keras-seq2seq.py
+++ model3/keras-seq2seq.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 30 18:50:17 2021
+Created on Tue Mar 30 06:08:44 2021
 
 @author: prakh
 """
-# Import libraries
+
+# Reference : https://stackabuse.com/python-for-nlp-neural-machine-translation-with-seq2seq-in-keras/
+
+# Import relevant libraries
 import os, sys
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, LSTM, GRU, Dense, Embedding
@@ -15,7 +18,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Parameter selection
+np.__version__
+
+
 BATCH_SIZE = 64
 EPOCHS = 30
 LSTM_NODES =256
@@ -29,7 +34,6 @@ input_sentences = []
 output_sentences = []
 output_sentences_inputs = []
 
-# Read training file
 df = pd.read_csv('train.csv',header=None)
 
 for i in range(df.shape[0]):
@@ -50,24 +54,25 @@ print(input_sentences[0])
 print(output_sentences[0])
 print(output_sentences_inputs[0])
 
-# Read eval file
 df2 = pd.read_csv('eval.csv',header=None)
 eval_inputs = []
 for i in range(df2.shape[0]):
     eval_inputs.append(df2[0][i][:-1])
 
 total_input = input_sentences + eval_inputs
-print("Total length : ",len(total_input))
-
+len(total_input)
+len(input_sentences)
 
 # Tokenization and padding
 """ It divides a sentence into the corresponding list of word
 Then it converts the words to integers """
 input_tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
-#input_tokenizer.fit_on_texts(input_sentences)
+input_tokenizer.fit_on_texts(input_sentences)
 input_tokenizer.fit_on_texts(total_input)
 input_integer_seq = input_tokenizer.texts_to_sequences(input_sentences)
-print("Ex : ",input_tokenizer.word_index['size'],input_tokenizer.word_index['xbox'],input_tokenizer.word_index['madrasah'])
+
+#input_tokenizer.word_index['size']
+
 
 word2idx_inputs = input_tokenizer.word_index
 print('Total unique words in the input: %s' % len(word2idx_inputs))
@@ -102,6 +107,7 @@ decoder_output_sequences = pad_sequences(output_integer_seq, maxlen=max_out_len,
 
 
 # Word Embeddings
+# word embeddings capture a lot more information about words
 """ With integer reprensentation, a word is represented only with a single integer. 
 With vector representation a word is represented by a vector of 50, 100, 200, or 
 whatever dimensions you like. Hence, word embeddings capture a lot more information about words. 
@@ -110,8 +116,8 @@ from numpy import array
 from numpy import asarray
 from numpy import zeros
 
-# Glove vector embeddings
 embeddings_dictionary = dict()
+
 glove_file = open(r'glove.6B.100d.txt', encoding="utf8")
 
 for line in glove_file:
@@ -152,27 +158,28 @@ for i, d in enumerate(decoder_output_sequences):
     for t, word in enumerate(d):
         decoder_targets_one_hot[i, t, word] = 1
 
-# Stacked encoder-decoder
-# Encoder
+
 encoder_inputs_placeholder = Input(shape=(max_input_len,))
 x = embedding_layer(encoder_inputs_placeholder)
-encoder1 = LSTM(LSTM_NODES, return_state= True, return_sequences=True)
 encoder = LSTM(LSTM_NODES, return_state=True)
-encoder_outputs, h, c = encoder(encoder1(x))
+
+
+encoder_outputs, h, c = encoder(x)
 encoder_states = [h, c]
 
-# Decoder
+
 decoder_inputs_placeholder = Input(shape=(max_out_len,))
 decoder_embedding = Embedding(num_words_output, LSTM_NODES)
 decoder_inputs_x = decoder_embedding(decoder_inputs_placeholder)
-decoder = LSTM(LSTM_NODES, return_sequences=True, return_state=True)
+
 decoder_lstm = LSTM(LSTM_NODES, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder(decoder_inputs_x, initial_state=encoder_states))
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs_x, initial_state=encoder_states)
+
 decoder_dense = Dense(num_words_output, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
 model = Model([encoder_inputs_placeholder,
-  decoder_inputs_placeholder], decoder_outputs)  
+  decoder_inputs_placeholder], decoder_outputs)
 model.compile(
     optimizer='rmsprop',
     loss='categorical_crossentropy',
@@ -188,7 +195,6 @@ r = model.fit(
     validation_split=0.1,
 )
 
-
 plt.plot(r.history["loss"], label="Training Loss")
 plt.plot(r.history["val_loss"], label="Validation Loss")
 plt.legend()
@@ -201,15 +207,18 @@ plt.legend()
 plt.show()
 
 encoder_model = Model(encoder_inputs_placeholder, encoder_states)
+
 decoder_state_input_h = Input(shape=(LSTM_NODES,))
 decoder_state_input_c = Input(shape=(LSTM_NODES,))
 decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
 decoder_inputs_single = Input(shape=(1,))
 decoder_inputs_single_x = decoder_embedding(decoder_inputs_single)
-decoder_outputs, state_h, state_c = decoder(decoder_inputs_single_x, initial_state=decoder_states_inputs)
-decoder_outputs2, h, c = decoder_lstm(decoder(decoder_inputs_single_x, initial_state=[state_h, state_c]))
+
+decoder_outputs, h, c = decoder_lstm(decoder_inputs_single_x, initial_state=decoder_states_inputs)
+
 decoder_states = [h, c]
-decoder_outputs = decoder_dense(decoder_outputs2)
+decoder_outputs = decoder_dense(decoder_outputs)
 
 decoder_model = Model(
     [decoder_inputs_single] + decoder_states_inputs,
@@ -245,7 +254,6 @@ def translate_sentence(input_seq):
 
     return ' '.join(output_sentence)
 
-# Selecting random string from train
 i = np.random.choice(len(input_sentences))
 input_seq = encoder_input_sequences[i:i+1]
 translation = translate_sentence(input_seq)
@@ -253,12 +261,12 @@ print('-')
 print('Input:', input_sentences[i])
 print('Response:', translation)
 
-"""
+
+
 word2idx_inputs["was"]
 word2idx_inputs["the"]
 word2idx_inputs["landing"]
 word2idx_inputs["successful"]
-"""
 
 def encode_to_input(s):
     x  = []
@@ -268,23 +276,11 @@ def encode_to_input(s):
     
 translation = translate_sentence(encode_to_input("What kind of glass exists in nature"))
 print('Response:', translation)
-# Response: what in the new rainforests
+# Response: what is the university of force of chicago
 
 translation = translate_sentence(encode_to_input("was the landing successful"))
 print('Response:', translation)
-# Response: of the rainforests
+# Response: who was the first doctor
 
-translation = translate_sentence(encode_to_input("what county is raleigh in"))
-print('Response:', translation)
-# Response: of the rainforests
-
-
-
-"""
-Reference : 
-    1. https://stackabuse.com/python-for-nlp-neural-machine-translation-with-seq2seq-in-keras/
-    2. https://stackoverflow.com/questions/52465971/keras-seq2seq-stacked-layers (for stacking)
-    3. https://arxiv.org/pdf/1610.03098.pdf (Similiar paper: Neural Paraphrase Generation with Stacked Residual LSTM Networks)
-"""
 
 
